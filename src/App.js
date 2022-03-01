a7c6b683cbe8713b64ecdd900abeb818e7c2b119
nyonyojoe <nyonyojoe@gmail.com>
2022-03-01 19:35:23 +0000

    ---

--- src/App.js
+++ src/App.js
@@ -3,18 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import styled from 'styled-components'
 import Login from './components/Login'
+import LoginPage from './pages/LoginPage'
 
 const App = () => {
   return (
-    <Router>
-    <Container>
-      <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/login' element={<h1>LoginPage</h1>} />
-          <Route path='/signup' element={<h1>Signup Page</h1>} />
-      </Routes>
-    </Container>
-</Router>
+    <LoginPage />
   )
 }
 
